57fb585ad4bad9c9a632ec3d384082f376e8cf09
vinod <orugantivinod7772gmail.com>
2025-06-17 14:56:19 +0530

    for the bookmark code

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.static('public')); 
 
